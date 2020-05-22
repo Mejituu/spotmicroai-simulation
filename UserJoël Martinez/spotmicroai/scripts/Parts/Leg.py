f46ea198905cc08078c9991de43d8710e6d8aad4
Joël Martínez <joel.martinez@alumni.mondragon.edu>
2020-05-22 18:22:58 +0200

    Added ROS nodes for servos=Optimized servo control

--- UserJoël Martinez/spotmicroai/scripts/Parts/Leg.py
+++ UserJoël Martinez/spotmicroai/scripts/Parts/Leg.py
@@ -3,41 +3,33 @@ import rospy
 import sys
 from enum import Enum
 from std_msgs.msg import Float64
-from spotmicroai.msg import LegCommand
+from spotmicroai.msg import LegCommand, ServoCommand
 
-
-def MoveLeg(data, number):
+#Callback for the subscriber
+def MoveLeg(data):
+    Verbose=0
     if data.Legs[number]==1:
-        #Create the motor publishers
-        pub1 = rospy.Publisher('spotmicroai/'+name+'_shoulder_position_controller/command', Float64,queue_size=10)
-        pub2 = rospy.Publisher('spotmicroai/'+name+'_leg_position_controller/command', Float64,queue_size=10)
-        pub3 = rospy.Publisher('spotmicroai/'+name+'_foot_position_controller/command', Float64,queue_size=10)
-
 
         rate = rospy.Rate(100) # 100 Hz
-        print("[ INFO] Moving "+ name)
+        if Verbose==1:
+            print("[ INFO] Moving "+ name)
+        pub.publish(data.Angles)
+        rate.sleep()
 
-        for i in range (0,200):
-            pub1.publish(data.Theta1)
-            pub2.publish(data.Theta2)
-            pub3.publish(data.Theta3)
-            rate.sleep()
-
-def Leg(name, number):
-
-    #Create the subscriber for the comands
-    rospy.Subscriber('spotmicroai/MoveLegs', LegCommand, MoveLeg, number)
-
-    rospy.spin()
-    
-            
 if __name__ == '__main__':
 
     #Initialize the node
     rospy.init_node("~name", anonymous=True)
 
     #Get node name
-    name = rospy.get_param('~name', 'Leg')
+    name = rospy.get_param('~name', 'Leg_0')
     number = rospy.get_param('~number', 0)
     print("[ INFO] "+name +" node initialized")
-    Leg(name, number)
+
+    #Create the Servo publishers
+    pub = rospy.Publisher('spotmicroai/MoveLegs/'+name, ServoCommand,queue_size=10)
+
+    #Create the subscriber for the comands
+    rospy.Subscriber('spotmicroai/MoveLegs', LegCommand, MoveLeg)
+
+    rospy.spin()
--- UserJoël Martinez/spotmicroai/scripts/Sit.py
+++ UserJoël Martinez/spotmicroai/scripts/Sit.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, String
 import time
 from Helpers.kinematics import Kinematic
+from Helpers.Input import Input
 from spotmicroai.msg import LegCommand
-from Parts.Leg import Leg
 from enum import Enum
 
+#TODO: global sucks
+global key
 
+#Classes definitions
 class Positions():
     Agachado=[-55,-100,20]
     Arriba=[-55,-190,20]
@@ -34,60 +37,47 @@ class movements:
 
         #Prepare data for the msg
         msg=LegCommand()
-        msg.Theta1=angles[0]
-        msg.Theta2=angles[1]
-        msg.Theta3=angles[2]
+        msg.Angles[0]=angles[0]
+        msg.Angles[1]=angles[1]
+        msg.Angles[2]=angles[2]
 
         msg.Legs=Legs
         #publish the msg
         self.MoveLegs.publish(msg)
 
+
+#function definitions
+def JoyStick_callback(data):
+    global key
+    key=data.data
+
+
+#TEST!
 movimiento = movements()
 Posiciones=Positions()
-SIT=1
-UP=0
-DOWN=0
+Inputs=Input()
+
+global key
+
+key=" "
+last=key
+rospy.Subscriber("JoyStick", String, JoyStick_callback)
 rospy.sleep(1)
-if SIT:
-    Legs=[0,0,1,1]
-    movimiento.Move(Legs,Posiciones.SitBack)
-    Legs=[1,1,0,0]
-    movimiento.Move(Legs,Posiciones.SitFront)
-if UP:
-    Legs=[1,1,1,1]
-    movimiento.Move(Legs,Posiciones.Arriba)
-if DOWN:
-    Legs=[1,1,1,1]
-    movimiento.Move(Legs,Posiciones.Agachado)
+while not rospy.is_shutdown():
 
-rospy.sleep(3)
-SIT=0
-UP=0
-DOWN=1
-if SIT:
-    Legs=[0,0,1,1]
-    movimiento.Move(Legs,Posiciones.SitBack)
-    Legs=[1,1,0,0]
-    movimiento.Move(Legs,Posiciones.SitFront)
-if UP:
-    Legs=[1,1,1,1]
-    movimiento.Move(Legs,Posiciones.Arriba)
-if DOWN:
-    Legs=[1,1,1,1]
-    movimiento.Move(Legs,Posiciones.Agachado)
+    if key!=last and key !="stop":
+        print(key)
+        if key=="right":
+            Legs=[0,0,1,1]
+            movimiento.Move(Legs,Posiciones.SitBack)
+            Legs=[1,1,0,0]
+            movimiento.Move(Legs,Posiciones.SitFront)
+        elif key=="up":
+            Legs=[1,1,1,1]
+            movimiento.Move(Legs,Posiciones.Arriba)
+        elif key=="down":
+            Legs=[1,1,1,1]
+            movimiento.Move(Legs,Posiciones.Agachado)
+        last=key
+    
 
-rospy.sleep(4)
-SIT=0
-UP=1
-DOWN=0
-if SIT:
-    Legs=[0,0,1,1]
-    movimiento.Move(Legs,Posiciones.SitBack)
-    Legs=[1,1,0,0]
-    movimiento.Move(Legs,Posiciones.SitFront)
-if UP:
-    Legs=[1,1,1,1]
-    movimiento.Move(Legs,Posiciones.Arriba)
-if DOWN:
-    Legs=[1,1,1,1]
-    movimiento.Move(Legs,Posiciones.Agachado)
